022f9d92623216df1d257da2e2f2227e404232dd
ousttrue <ousttrue@gmail.com>
2021-09-10 16:38:27 +0900

    overlay

--- example/gizmesh_example_d3d11/main.cpp
+++ example/gizmesh_example_d3d11/main.cpp
@@ -22,6 +22,56 @@ enum class transform_mode { translate, rotate, scale };
 #include <ranges>
 #include <vector>
 
+static void ShowExampleAppSimpleOverlay() {
+  static int corner = 0;
+  ImGuiIO &io = ImGui::GetIO();
+  ImGuiWindowFlags window_flags =
+      ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize |
+      ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing |
+      ImGuiWindowFlags_NoNav;
+  if (corner != -1) {
+    const float PAD = 10.0f;
+    const ImGuiViewport *viewport = ImGui::GetMainViewport();
+    ImVec2 work_pos =
+        viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
+    ImVec2 work_size = viewport->WorkSize;
+    ImVec2 window_pos, window_pos_pivot;
+    window_pos.x =
+        (corner & 1) ? (work_pos.x + work_size.x - PAD) : (work_pos.x + PAD);
+    window_pos.y =
+        (corner & 2) ? (work_pos.y + work_size.y - PAD) : (work_pos.y + PAD);
+    window_pos_pivot.x = (corner & 1) ? 1.0f : 0.0f;
+    window_pos_pivot.y = (corner & 2) ? 1.0f : 0.0f;
+    ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+    window_flags |= ImGuiWindowFlags_NoMove;
+  }
+  ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+  if (ImGui::Begin("Example: Simple overlay", nullptr, window_flags)) {
+    ImGui::Text("Simple overlay\n"
+                "in the corner of the screen.\n"
+                "(right-click to change position)");
+    ImGui::Separator();
+    if (ImGui::IsMousePosValid())
+      ImGui::Text("Mouse Position: (%.1f,%.1f)", io.MousePos.x, io.MousePos.y);
+    else
+      ImGui::Text("Mouse Position: <invalid>");
+    if (ImGui::BeginPopupContextWindow()) {
+      if (ImGui::MenuItem("Custom", NULL, corner == -1))
+        corner = -1;
+      if (ImGui::MenuItem("Top-left", NULL, corner == 0))
+        corner = 0;
+      if (ImGui::MenuItem("Top-right", NULL, corner == 1))
+        corner = 1;
+      if (ImGui::MenuItem("Bottom-left", NULL, corner == 2))
+        corner = 2;
+      if (ImGui::MenuItem("Bottom-right", NULL, corner == 3))
+        corner = 3;
+      ImGui::EndPopup();
+    }
+  }
+  ImGui::End();
+}
+
 //////////////////////////
 //   Main Application   //
 //////////////////////////
@@ -98,9 +148,10 @@ int main(int argc, char *argv[]) {
     // ImGui_ImplWin32_NewFrame();
     ImGuiIO &io = ImGui::GetIO();
     io.DisplaySize = ImVec2(state.Width, state.Height);
+    io.MousePos = ImVec2(state.MouseX, state.MouseY);
 
     ImGui::NewFrame();
-    ImGui::ShowDemoWindow();
+    ShowExampleAppSimpleOverlay();
     ImGui::Render();
 
     // update camera
